c8b3a8213eed54975f428eab55529e06d616e8c5
gytkk <gytk.kim@gmail.com>
2025-06-27 12:39:41 +0900

    modules/devsisters에 eclair 추가

--- modules/devsisters/default.nix
+++ modules/devsisters/default.nix
@@ -5,11 +5,36 @@
   ...
 }:
 
+let
+  eclair = pkgs.writeShellScriptBin "ecl" ''
+    export PATH="/Users/gyutak/.gem/ruby/3.1.0/bin:${pkgs.ruby_3_1}/bin:${pkgs.tmux}/bin:$PATH"
+    export GEM_PATH="${pkgs.ruby_3_1}/lib/ruby/gems/3.1.0:/Users/gyutak/.gem/ruby/3.1.0"
+    export GEM_HOME="/Users/gyutak/.gem/ruby/3.1.0"
+
+    # Install eclair gem if not present
+    if ! ${pkgs.ruby_3_1}/bin/gem list ecl -i > /dev/null 2>&1; then
+      echo "Installing eclair gem..."
+      ${pkgs.ruby_3_1}/bin/gem install ecl --version 3.0.4 --user-install --no-document 2>/dev/null
+    fi
+
+    # Execute the actual ecl command
+    exec "/Users/gyutak/.gem/ruby/3.1.0/bin/ecl" "$@"
+  '';
+in
 {
   home.packages = with pkgs; [
     # Authentication
     saml2aws
     vault
+
+    # Required dependencies for DevSisters tools
+    ruby_3_1
+    tmux
+    ncurses.dev
+    gcc
+
+    # DevSisters tools
+    eclair
 
     # Custom scripts
     (pkgs.writeShellScriptBin "sign" (builtins.readFile ./scripts/sign))
