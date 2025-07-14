ce473f9f5cd99705e4440a1087090c7c67631353
gytkk <gytk.kim@gmail.com>
2025-07-14 09:36:24 +0900

    Fix a typo in modules/vim

--- modules/vim/default.nix
+++ modules/vim/default.nix
@@ -19,7 +19,7 @@
       filetype plugin indent on
 
       " Editor settings
-      set endcoding=utf-8
+      set encoding=utf-8
       set fileencoding=utf-8
       set termencoding=utf-8
       set mouse=a
