72827464dcc05b466bc51bdb2d736c416c393fc6
gytkk <gytk.kim@gmail.com>
2025-08-07 08:51:13 +0900

    Update zsh.programs.initContent to run nix-daemon

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -112,7 +112,7 @@
       fi
     '';
 
-    initExtra = ''
+    initContent = ''
       if [[ ! $(command -v nix) && -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]]; then
         source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
       fi
