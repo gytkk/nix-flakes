b3d42a68a32e899f252d68306c794a9974bb86a5
gytkk <gytk.kim@gmail.com>
2025-12-07 21:57:19 +0900

    Remove sepia.nix file

--- hosts/pylv-sepia/configuration.nix
+++ hosts/pylv-sepia/configuration.nix
@@ -3,7 +3,7 @@
   lib,
   pkgs,
   ...
-} @ args:
+}@args:
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
@@ -26,14 +26,25 @@
     htop
     tmux
     less
+    direnv
 
     # Development
     git
+    git-lfs
     gh
+    delta
+    lazygit
     vim
     jq
     ripgrep
     fd
+    fzf
+    bat
+
+    # Zsh
+    zsh-powerlevel10k
+    zsh-autosuggestions
+    zsh-syntax-highlighting
 
     # Python
     uv
@@ -55,15 +66,74 @@
     code-server
   ];
 
-  security.sudo.enable = true;
+  # Zsh
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" "docker" "kubectl" "z" "fzf" ];
+    };
+    promptInit = ''
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+    '';
+    shellAliases = {
+      ls = "ls --color=auto";
+      ll = "ls -la --color=auto";
+      vim = "nvim";
+      vi = "nvim";
+      kl = "kubectl";
+      kx = "kubectx";
+    };
+  };
+
+  # Git system-wide config
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    config = {
+      user = {
+        name = "gytkk";
+        email = "gytk.kim@gmail.com";
+      };
+      core = {
+        editor = "vim";
+        pager = "delta";
+      };
+      interactive.diffFilter = "delta --color-only";
+      delta = {
+        navigate = true;
+        side-by-side = true;
+        line-numbers = true;
+      };
+      pull.rebase = true;
+      push = {
+        default = "current";
+        autoSetupRemote = true;
+      };
+      init.defaultBranch = "main";
+      fetch = {
+        prune = true;
+        pruneTags = true;
+      };
+    };
+  };
+
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  };
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJhE4Uakcz7usa0aetMqb99LYybOQ0I+sWKOiAidmBio gytk.kim@gmail.com"
-  ] ++ (args.extraPublicKeys or []);
+  ]
+  ++ (args.extraPublicKeys or [ ]);
 
   users.users.gytkk = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+    shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJhE4Uakcz7usa0aetMqb99LYybOQ0I+sWKOiAidmBio gytk.kim@gmail.com"
     ];
