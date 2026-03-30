f9a24bc17cf133588ccd0ceda4edb47f3e24b33c
Gyutak Kim <gyutak.kim@devsisters.com>
2026-03-30 10:32:34 +0900

    feat: auto-start zellij in ghostty shells

--- modules/zellij/default.nix
+++ modules/zellij/default.nix
@@ -1,4 +1,8 @@
-{ ... }:
+{
+  config,
+  lib,
+  ...
+}:
 
 {
   programs.zellij = {
@@ -8,4 +12,16 @@
       theme = "ayu-light";
     };
   };
+
+  programs.zsh.initContent = lib.mkAfter ''
+    if [[ -o interactive ]] \
+      && [[ -z "$ZELLIJ" ]] \
+      && [[ -z "$TMUX" ]] \
+      && [[ -z "$SSH_CONNECTION" ]] \
+      && [[ -z "$SSH_CLIENT" ]] \
+      && [[ -z "$SSH_TTY" ]] \
+      && { [[ "$TERM_PROGRAM" == "ghostty" ]] || [[ "$TERM_PROGRAM" == "xterm-ghostty" ]] || [[ "$TERM" == "xterm-ghostty" ]] || [[ -n "$GHOSTTY_RESOURCES_DIR" ]]; }; then
+      exec ${lib.getExe config.programs.zellij.package}
+    fi
+  '';
 }
