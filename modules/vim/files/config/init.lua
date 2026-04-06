4918c96570661d1c8ef993fd6d26646f7cefe4be
Gyutak Kim <gyutak.kim@devsisters.com>
2026-04-06 17:44:29 +0900

    fix(vim): change blink.cmp keymap preset to super-tab for Tab completion
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- modules/vim/files/config/init.lua
+++ modules/vim/files/config/init.lua
@@ -265,7 +265,7 @@ local blink = {
   dependencies = { "rafamadriz/friendly-snippets" },
   event = "InsertEnter",
   opts = {
-    keymap = { preset = "default" },
+    keymap = { preset = "super-tab" },
     appearance = { nerd_font_variant = "mono" },
     completion = {
       documentation = { auto_show = true, auto_show_delay_ms = 300 },
