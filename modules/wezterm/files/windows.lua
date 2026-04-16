f69321003daffb6cf091ca3c2716451fe2914bda
gytkk <gytk.kim@gmail.com>
2026-04-16 23:08:13 +0900

    fix: hide Windows wezterm title bar

--- modules/wezterm/files/windows.lua
+++ modules/wezterm/files/windows.lua
@@ -4,6 +4,7 @@ local shared = require 'shared'
 local config = wezterm.config_builder()
 
 shared.apply_to_config(config, wezterm)
+config.window_decorations = 'RESIZE'
 config.default_domain = 'WSL:Ubuntu'
 
 return config
