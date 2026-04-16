28de25df7f8bef33dbe280a47521dd7acfbd5517
Gyutak Kim <gyutak.kim@devsisters.com>
2026-04-16 13:44:48 +0900

    fix: force one-half-light active wezterm tab

--- modules/wezterm/files/wezterm.lua
+++ modules/wezterm/files/wezterm.lua
@@ -1,6 +1,44 @@
 local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
+local active_tab_bg = '#fdfdfd'
+local active_tab_fg = '#383a42'
+local tab_bar_bg = '#eef1f4'
+local inactive_tab_bg = '#e4e9ef'
+local inactive_tab_hover_bg = '#dde3ea'
+local inactive_tab_fg = '#5c6370'
+
+local function tab_title(tab)
+  local title = tab.tab_title
+  if title ~= nil and #title > 0 then
+    return title
+  end
+  return tab.active_pane.title
+end
+
+wezterm.on('format-tab-title', function(tab, _, _, _, hover, max_width)
+  local title = tab_title(tab)
+  if max_width ~= nil then
+    title = wezterm.truncate_right(title, math.max(max_width - 2, 1))
+  end
+
+  local bg = inactive_tab_bg
+  local fg = inactive_tab_fg
+
+  if tab.is_active then
+    bg = active_tab_bg
+    fg = active_tab_fg
+  elseif hover then
+    bg = inactive_tab_hover_bg
+    fg = active_tab_fg
+  end
+
+  return {
+    { Background = { Color = bg } },
+    { Foreground = { Color = fg } },
+    { Text = ' ' .. title .. ' ' },
+  }
+end)
 
 config.term = 'xterm-256color'
 
@@ -20,10 +58,10 @@ config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 
 config.window_frame = {
-  active_titlebar_bg = '#eef1f4',
-  inactive_titlebar_bg = '#eef1f4',
-  active_titlebar_fg = '#383a42',
-  inactive_titlebar_fg = '#5c6370',
+  active_titlebar_bg = tab_bar_bg,
+  inactive_titlebar_bg = tab_bar_bg,
+  active_titlebar_fg = active_tab_fg,
+  inactive_titlebar_fg = inactive_tab_fg,
   active_titlebar_border_bottom = '#d0d7de',
   inactive_titlebar_border_bottom = '#d0d7de',
 }
@@ -59,27 +97,27 @@ config.colors = {
     '#ffffff',
   },
   tab_bar = {
-    background = '#eef1f4',
+    background = tab_bar_bg,
     inactive_tab_edge = '#d0d7de',
     active_tab = {
-      bg_color = '#fdfdfd',
-      fg_color = '#383a42',
+      bg_color = active_tab_bg,
+      fg_color = active_tab_fg,
     },
     inactive_tab = {
-      bg_color = '#e4e9ef',
-      fg_color = '#5c6370',
+      bg_color = inactive_tab_bg,
+      fg_color = inactive_tab_fg,
     },
     inactive_tab_hover = {
-      bg_color = '#dde3ea',
-      fg_color = '#383a42',
+      bg_color = inactive_tab_hover_bg,
+      fg_color = active_tab_fg,
     },
     new_tab = {
-      bg_color = '#eef1f4',
-      fg_color = '#5c6370',
+      bg_color = tab_bar_bg,
+      fg_color = inactive_tab_fg,
     },
     new_tab_hover = {
-      bg_color = '#dde3ea',
-      fg_color = '#383a42',
+      bg_color = inactive_tab_hover_bg,
+      fg_color = active_tab_fg,
     },
   },
 }
