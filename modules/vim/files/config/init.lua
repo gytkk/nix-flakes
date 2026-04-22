508263dbb6fcc9dee71916721927e1b48c937a91
Gyutak Kim <gyutak.kim@devsisters.com>
2026-04-22 09:27:12 +0900

    refactor: tidy neovim lsp setup

--- modules/vim/files/config/init.lua
+++ modules/vim/files/config/init.lua
@@ -329,38 +329,46 @@ local blink = {
   opts_extend = { "sources.default" },
 }
 
+local lspServers = { "nixd", "gopls", "rust_analyzer", "ts_ls", "terraformls", "yamlls", "marksman", "ty" }
+
+local function setupLspDiagnostics()
+  vim.diagnostic.config({
+    virtual_lines = { current_line = true },
+  })
+
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous Diagnostic" })
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
+  vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+  vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostics List" })
+end
+
+local function onLspAttach(args)
+  local client = vim.lsp.get_client_by_id(args.data.client_id)
+  local opts = { buffer = args.buf }
+
+  if client and client:supports_method("textDocument/inlayHint") then
+    vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+  end
+
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+end
+
 local lspconfig = {
   "neovim/nvim-lspconfig",
   dependencies = { "saghen/blink.cmp" },
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    local capabilities = require("blink.cmp").get_lsp_capabilities()
-    local servers = { "nixd", "gopls", "rust_analyzer", "ts_ls", "terraformls", "yamlls", "marksman", "ty" }
-    for _, server in ipairs(servers) do
-      vim.lsp.config(server, { capabilities = capabilities })
-    end
-    vim.lsp.enable(servers)
-
-    vim.diagnostic.config({
-      virtual_lines = { current_line = true },
+    vim.lsp.config("*", {
+      capabilities = require("blink.cmp").get_lsp_capabilities(),
     })
 
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous Diagnostic" })
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
-    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
-    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostics List" })
+    vim.lsp.enable(lspServers)
+    setupLspDiagnostics()
 
     vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        local opts = { buffer = args.buf }
-        if client and client:supports_method("textDocument/inlayHint") then
-          vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
-        end
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-      end,
+      callback = onLspAttach,
     })
   end,
 }
