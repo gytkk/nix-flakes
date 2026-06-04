b247a1810c4eac88e719675e0e12b8f3018e5c32
Gyutak Kim <gyutak.kim@devsisters.com>
2026-06-04 15:14:45 +0900

    feat(tmux): add session manager wrapper

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -7,10 +7,17 @@
 
 let
   mkSymlink = path: config.lib.file.mkOutOfStoreSymlink "${flakeDirectory}/modules/tmux/${path}";
+  tmuxWrapper = pkgs.writeShellScriptBin "tmux" ''
+    if [ "$#" -eq 0 ] && [ -t 0 ] && [ -t 1 ] && [ -z "''${TMUX:-}" ]; then
+      exec ${pkgs.bash}/bin/bash ${flakeDirectory}/modules/tmux/files/tmux-session-manager.sh ${pkgs.tmux}/bin/tmux
+    fi
+
+    exec ${pkgs.tmux}/bin/tmux "$@"
+  '';
 in
 {
   home.packages = [
-    pkgs.tmux
+    tmuxWrapper
   ];
 
   xdg.configFile."tmux/tmux.conf".source = mkSymlink "files/tmux.conf";
