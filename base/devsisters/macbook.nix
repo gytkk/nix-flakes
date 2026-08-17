376c10b1ed937fe02b810a294a0ab2897967d430
gytkk <gytk.kim@gmail.com>
2026-08-17 21:11:46 +0900

    feat(macbook): replace Karabiner with hidutil

--- base/devsisters/macbook.nix
+++ base/devsisters/macbook.nix
@@ -1,7 +1,28 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  applyKeyboardMapping = pkgs.writeShellScript "apply-keyboard-mapping" ''
+    exec /usr/bin/hidutil property --set '{
+      "UserKeyMapping": [
+        {
+          "HIDKeyboardModifierMappingSrc": 0x700000039,
+          "HIDKeyboardModifierMappingDst": 0x7000000E0
+        },
+        {
+          "HIDKeyboardModifierMappingSrc": 0x7000000E7,
+          "HIDKeyboardModifierMappingDst": 0x700000068
+        }
+      ]
+    }'
+  '';
+in
 {
-  imports = [ ../../modules/karabiner ];
-
-  modules.karabiner.enable = true;
+  launchd.agents.hidutil-keyboard-mapping = {
+    enable = true;
+    config = {
+      ProgramArguments = [ "${applyKeyboardMapping}" ];
+      RunAtLoad = true;
+      ProcessType = "Interactive";
+    };
+  };
 }
